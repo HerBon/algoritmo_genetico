b5433804b549817a4bf2f6f65dd2db4f8e98e8c7
HerBon <herediabonifazcristianeduardo@gmail.com>
2021-05-12 02:28:49 -0400

    r

--- alg_genet.py
+++ alg_genet.py
@@ -1,15 +1,33 @@
 import random
 
 
-class personas:
+class poblacion:
     def __init__(self, estado, proba_vida):
         self.estado = estado
         self.proba_vida = proba_vida
 
+    def seleccionar_mejor(self, problacion1):
+        
+        
+        pass
+
+
+
+
     
 def generar_poblacion():
+    poblacion1 = []
+    for f in range(8):
+        poblacion1.append([])
+        for c in range(8):
+            poblacion1[f].append(random.randint(0, 1))
+    return poblacion1
 
 
+
+#print(generar_poblacion())
+'''
 person1= personas([1,1,0,0,0,1],0.1)
 
 print(person1.estado)
+'''
